17a301711c3b10b99e63f88109550fc129846707
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2020-04-03 09:41:00 +0200

    Create software.sh

---
+++ software.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+
+#sudo dnf install xorg-x11-drv-nvidia-cuda
+#sudo dnf install obs-studio

bd914e1e7ce042bda85ad015f570ac4a5e473204
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2020-04-14 09:01:36 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -5,8 +5,8 @@ sudo dnf groupinstall "Audio Production" -y
 sudo dnf groupinstall "Neuron Modelling Simulators" -y
 sudo dnf groupinstall "RPM Development Tools" -y
 
-sudo dnf install rawtherapee gnome-raw-thumbnailer gnome-epub-thumbnailer ufraw -y
-sudo dnf install blender krita gimp inkscape scribus -y
+sudo dnf install rawtherapee gnome-raw-thumbnailer gnome-epub-thumbnailer ufraw fltk fltk-devel gnome-tweaks -y
+sudo dnf install blender krita gimp inkscape scribus java-latest-openjdk-devel cmake clang llvm qtcreator -y
 sudo dnf install skopeo buildah podman virt-manager qemu-kvm ansible -y
 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm

12b4f1bdf55c41360c62f2f8bd63ad01df0be8b2
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2020-04-20 13:35:29 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -5,12 +5,17 @@ sudo dnf groupinstall "Audio Production" -y
 sudo dnf groupinstall "Neuron Modelling Simulators" -y
 sudo dnf groupinstall "RPM Development Tools" -y
 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+VERSION=1.14
+sudo dnf module enable cri-o:$VERSION
+sudo dnf install cri-o -y
 
-sudo dnf install no-more-secrets code-editor geteltorito genisoimage texlive-* -y
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+    
+sudo dnf install vlc no-more-secrets code-editor geteltorito genisoimage texlive-* -y
 sudo dnf install rawtherapee gnome-raw-thumbnailer gnome-epub-thumbnailer ufraw fltk fltk-devel gnome-tweaks -y
 sudo dnf install blender krita gimp inkscape scribus java-latest-openjdk-devel cmake clang llvm qtcreator -y
-sudo dnf install skopeo buildah podman virt-manager qemu-kvm ansible -y
+sudo dnf install skopeo buildah podman virt-manager qemu-kvm ansible nodejs postgres -y
 
 
 sudo dnf install dotnet-sdk-3.0 -y

30b08e7dcaae9eb01490012dc671f49d95f57389
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2020-04-20 16:27:56 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -12,7 +12,7 @@ sudo dnf install cri-o -y
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
     
-sudo dnf install vlc no-more-secrets code-editor geteltorito genisoimage texlive-* -y
+sudo dnf install vlc no-more-secrets code-editor geteltorito genisoimage texlive-* powershell -y
 sudo dnf install rawtherapee gnome-raw-thumbnailer gnome-epub-thumbnailer ufraw fltk fltk-devel gnome-tweaks -y
 sudo dnf install blender krita gimp inkscape scribus java-latest-openjdk-devel cmake clang llvm qtcreator -y
 sudo dnf install skopeo buildah podman virt-manager qemu-kvm ansible nodejs postgres -y
@@ -30,10 +30,13 @@ sudo dnf install dotnet-sdk-3.0 -y
 #sudo dnf install obs-studio
 
 sudo flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf install coreos-installer -y
+
+
 flatpak install flathub io.dbeaver.DBeaverCommunity -y
 flatpak install flathub com.visualstudio.code
 flatpak install flathub org.gnome.Builder
 
-sudo dnf install coreos-installer -y
+
 coreos-installer download --stream stable --platform qemu --decompress  --format qcow2.xz
 sudo mv fedora-coreos-qemu.qcow2 /var/lib/libvirt/images/fedora-coreos-qemu.qcow2

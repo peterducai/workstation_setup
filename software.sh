9ed7fb9a394f2c41fbfc8ce541a391e20d16f139
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2020-04-20 13:19:08 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -5,15 +5,13 @@ sudo dnf groupinstall "Audio Production" -y
 sudo dnf groupinstall "Neuron Modelling Simulators" -y
 sudo dnf groupinstall "RPM Development Tools" -y
 
-sudo dnf install no-more-secrets code-editor texlive-* -y
-sudo dnf install geteltorito genisoimage
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-
+sudo dnf install no-more-secrets code-editor geteltorito genisoimage texlive-* -y
 sudo dnf install rawtherapee gnome-raw-thumbnailer gnome-epub-thumbnailer ufraw fltk fltk-devel gnome-tweaks -y
 sudo dnf install blender krita gimp inkscape scribus java-latest-openjdk-devel cmake clang llvm qtcreator -y
 sudo dnf install skopeo buildah podman virt-manager qemu-kvm ansible -y
 
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf install dotnet-sdk-3.0 -y
 

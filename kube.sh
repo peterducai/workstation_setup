0c1246373a38979508b45d989fb75cef9979646e
pducai <pducai@redhat.com>
2020-04-28 10:58:16 +0200

    operator sdk

---
+++ kube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+wget https://github.com/operator-framework/operator-sdk/releases/download/v0.17.0/operator-sdk-v0.17.0-x86_64-linux-gnu
+sudo mv operator-sdk-v0.17-x86_64-linux-gnu /usr/local/bin/operator-sdk
+sudo chmod +x /usr/local/bin/operator-sdk

087e1c7199f22d4d025ca9c3f1a918927d1ac26b
peter ducai alias daemonna <peter.ducai@gmail.com>
2020-05-21 14:55:38 +0200

    kube

--- kube.sh
+++ kube.sh
@@ -2,6 +2,7 @@
 
 
 
-wget https://github.com/operator-framework/operator-sdk/releases/download/v0.17.0/operator-sdk-v0.17.0-x86_64-linux-gnu
-sudo mv operator-sdk-v0.17-x86_64-linux-gnu /usr/local/bin/operator-sdk
-sudo chmod +x /usr/local/bin/operator-sdk
+wget https://github.com/operator-framework/operator-sdk/releases/download/v0.17.1/operator-sdk-v0.17.1-x86_64-linux-gnu
+sudo mkdir -p /usr/local/bin/operator-sdk
+sudo mv operator-sdk-v0.17.1-x86_64-linux-gnu /usr/local/bin/operator-sdk
+sudo chmod +x /usr/local/bin/operator-sdk/operator-sdk-v0.17.1-x86_64-linux-gnu 

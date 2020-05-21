550810ff4a8d0e5b14cc42c39a2c4cb9b2b28079
pducai <pducai@localhost.localdomain>
2020-05-21 12:49:24 +0200

    x

--- software.sh
+++ software.sh
@@ -7,7 +7,7 @@ sudo dnf groupinstall "RPM Development Tools" -y
 
 VERSION=1.14
 sudo dnf module enable cri-o:$VERSION
-sudo dnf install cri-o -y
+sudo dnf install vim cri-o golang vim -y
 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
